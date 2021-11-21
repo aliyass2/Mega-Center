ccdd9bc0150fd1aeb2f46425a4a0b637c2e722e9
aliyass2 <ali.yass21@yahoo.com>
2021-11-21 18:38:19 +0300

    Mega-Center

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//هنا كود لفتح وغلق القائمة
+var navLinks = document.getElementById("navLinks");
+function showMenu() {
+  navLinks.style.right = "0";
+}
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
